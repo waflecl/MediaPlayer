ad1b332e1b6ed7f5cedb5c7d5d85a3ffbcd2f745
Eric Zepeda <ezepeda@wafle.cl>
2015-03-11 16:36:48 -0300

    Avances del desarrollo
    19-06-2014
    
    *Se vincula las acciones de los botones de la notificación

--- libraries/shutterbug/build/generated/source/buildConfig/debug/com/applidium/shutterbug/BuildConfig.java
+++ libraries/shutterbug/build/generated/source/buildConfig/debug/com/applidium/shutterbug/BuildConfig.java
@@ -5,7 +5,7 @@ package com.applidium.shutterbug;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.applidium.shutterbug";
+  public static final String APPLICATION_ID = "com.applidium.shutterbug";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- libraries/vitamio/build/generated/source/buildConfig/release/io/vov/vitamio/BuildConfig.java
+++ libraries/vitamio/build/generated/source/buildConfig/release/io/vov/vitamio/BuildConfig.java
@@ -5,7 +5,7 @@ package io.vov.vitamio;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "io.vov.vitamio";
+  public static final String APPLICATION_ID = "io.vov.vitamio";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 400;
